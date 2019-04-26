9546204bd6d10da98e683eb76e6cf18bf432a0ec
gbayarri <genis.bayarri@irbbarcelona.org>
2019-04-26 15:07:22 +0200

    v1.0.1 April 2019 Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_chemistry",
-    version="0.0.1",
+    version="1.0.1",
     author="Biobb developers",
     author_email="genis.bayarri@irbbarcelona.org",
     description="Biobb_md is the Biobb module collection to perform molecular dynamics simulations.",
@@ -18,11 +18,11 @@ setuptools.setup(
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs', 'test',]),
-    install_requires=['biobb_common>=0.0.8'],
-    python_requires='>=3',
+    install_requires=['biobb_common==0.1.4'],
+    python_requires='==3.6.*',
     classifiers=(
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
