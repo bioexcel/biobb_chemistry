420872058f761e7caa59152ad1cab228b714c99e
gbayarri <genis.bayarri@irbbarcelona.org>
2020-01-27 16:54:00 +0100

    Test autodocs

--- biobb_chemistry/acpype/acpype_params_ac.py
+++ biobb_chemistry/acpype/acpype_params_ac.py
@@ -16,7 +16,7 @@ class AcpypeParamsAC():
     `Visit the official page <https://github.com/alanwilter/acpype>`_.
 
     Args:
-        input_path (str): Path to the input file. Accepted formats: pdb, mdl, mol2.
+        input_path (str) [<a href="https://github.com/alanwilter/acpype">Sample file</a>]: Path to the input file. Accepted formats: pdb, mdl, mol2.
         output_path_frcmod (str): Path to the FRCMOD output file. Accepted formats: frcmod.
         output_path_inpcrd (str): Path to the INPCRD output file. Accepted formats: inpcrd.
         output_path_lib (str): Path to the LIB output file. Accepted formats: lib.
