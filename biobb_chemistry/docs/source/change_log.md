86b37fa073a813b64f28874ab6002171b8080eca
gbayarri <genis.bayarri@irbbarcelona.org>
2021-10-13 11:39:58 +0200

    2021.3

--- biobb_chemistry/docs/source/change_log.md
+++ biobb_chemistry/docs/source/change_log.md
@@ -1,5 +1,12 @@
 # Biobb Chemistry changelog
 
+## What's new in version [3.7.0](https://github.com/bioexcel/biobb_chemistry/releases/tag/v3.7.0)?
+In version 3.7.0 the dependency biobb_common has been updated to 3.7.0 version. 
+
+### New features
+
+* Update to biobb_common 3.7.0 (general)
+
 ## What's new in version [3.6.0](https://github.com/bioexcel/biobb_chemistry/releases/tag/v3.6.0)?
 In version 3.6.0 the dependency biobb_common has been updated to 3.6.0 version. 
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_chemistry",
-    version="3.6.0",
+    version="3.7.0",
     author="Biobb developers",
     author_email="genis.bayarri@irbbarcelona.org",
     description="Biobb_chemistry is the Biobb module collection to perform chemistry over molecular dynamics simulations.",
@@ -18,8 +18,22 @@ setuptools.setup(
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs', 'test']),
-    install_requires=['biobb_common==3.6.0'],
+    install_requires=['biobb_common==3.7.0'],
     python_requires='==3.7.*',
+    entry_points={
+        "console_scripts": [
+            "acpype_params_ac = biobb_chemistry.acpype.acpype_params_ac:main",
+            "acpype_params_cns = biobb_chemistry.acpype.acpype_params_cns:main",
+            "acpype_params_gmx_opls = biobb_chemistry.acpype.acpype_params_gmx_opls:main",
+            "acpype_params_gmx = biobb_chemistry.acpype.acpype_params_gmx:main",
+            "reduce_add_hydrogens = biobb_chemistry.ambertools.reduce_add_hydrogens:main",
+            "reduce_remove_hydrogens = biobb_chemistry.ambertools.reduce_remove_hydrogens:main",
+            "babel_add_hydrogens = biobb_chemistry.babelm.babel_add_hydrogens:main",
+            "babel_convert = biobb_chemistry.babelm.babel_convert:main",
+            "babel_minimize = biobb_chemistry.babelm.babel_minimize:main",
+            "babel_remove_hydrogens = biobb_chemistry.babelm.babel_remove_hydrogens:main"
+        ]
+    },
     classifiers=(
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.7",
