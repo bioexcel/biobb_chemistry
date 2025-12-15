2e9d2073b205fe95cade5f8e53dc743a24b6c295
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-12-15 12:42:22 +0100

    Remove old comments

--- biobb_chemistry/babelm/babel_remove_hydrogens.py
+++ biobb_chemistry/babelm/babel_remove_hydrogens.py
@@ -191,7 +191,6 @@ class BabelRemoveHydrogens(BiobbObject):
         self.copy_to_host()
 
         # remove temporary folder(s)
-        # self.tmp_files.extend([self.stage_io_dict.get("unique_dir", "")])
         self.remove_tmp_files()
 
         self.check_arguments(output_files_created=True, raise_exception=False)
